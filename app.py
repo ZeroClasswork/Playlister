225f81cd7e178f72d8e4dfbbc351eb2e0820e8dc
atleastzero <megan.obryan52@gmail.com>
2019-10-01 15:11:28 -0700

    Fixed typo

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ import os
 
 app = Flask(__name__)
 client = MongoClient()
-db = client.Playlister
+db = client.get_default_database()
 playlists = db.playlists
+comments = db.comments
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
@@ -73,6 +74,18 @@ def playlists_delete(playlist_id):
     """Delete one playlist."""
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+    """Submit a new comment."""
+    comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'playlist_id': ObjectId(request.form.get('playlist_id'))
+    }
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
